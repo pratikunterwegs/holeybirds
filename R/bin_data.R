fbac9644f687fb85a15761524ce32c1e28f09bde
Pratik Gupte <pratikgupte16@gmail.com>
2021-06-25 16:20:37 +0200

    styler

--- R/bin_data.R
+++ R/bin_data.R
@@ -9,21 +9,21 @@
 #' @return A dataframe with the axes binned and counted.
 #' @export
 #'
-bin_data = function(
-    data,
-    id_cols = "id",
-    axes = c("x", "y"),
-    binsize = 100
-) {
-    # set data.table
-    data.table::setDT(data)
-    # round data
-    data = data[, lapply(.SD, plyr::round_any,
-                            accuracy = binsize, f = floor),
-            .SDcols = axes, by = id_cols]
-    # count data per id and axis
-    data_summary = data[, list(.N), by = c(id_cols, axes)]
+bin_data <- function(data,
+                     id_cols = "id",
+                     axes = c("x", "y"),
+                     binsize = 100) {
+  # set data.table
+  data.table::setDT(data)
+  # round data
+  data <- data[, lapply(.SD, plyr::round_any,
+    accuracy = binsize, f = floor
+  ),
+  .SDcols = axes, by = id_cols
+  ]
+  # count data per id and axis
+  data_summary <- data[, list(.N), by = c(id_cols, axes)]
 
-    # return data
-    data_summary
+  # return data
+  data_summary
 }
