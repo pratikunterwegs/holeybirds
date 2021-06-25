5afde740d1dded5660087671496063e048c6573d
Pratik Gupte <pratikgupte16@gmail.com>
2021-06-25 16:07:22 +0200

    add documentation

--- R/bin_data.R
+++ R/bin_data.R
@@ -1,4 +1,14 @@
 
+#' Bin data.
+#'
+#' @param data The dataframe to bin.
+#' @param id_cols The grouping columns.
+#' @param axes The columns to bin.
+#' @param binsize The binsize.
+#'
+#' @return A dataframe with the axes binned and counted.
+#' @export
+#'
 bin_data = function(
     data,
     id_cols = "id",
@@ -8,8 +18,8 @@ bin_data = function(
     # set data.table
     data.table::setDT(data)
     # round data
-    data = data[, lapply(.SD, plyr::round_any, 
-                            accuracy = binsize, f = floor), 
+    data = data[, lapply(.SD, plyr::round_any,
+                            accuracy = binsize, f = floor),
             .SDcols = axes, by = id_cols]
     # count data per id and axis
     data_summary = data[, list(.N), by = c(id_cols, axes)]
