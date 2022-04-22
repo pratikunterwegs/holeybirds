00216602a58cfea6d647e605b5162c513cecf418
Pratik Gupte <pratikgupte16@gmail.com>
2022-04-22 14:47:44 +0200

    add supplement code as well

--- bash/copy_tex.bat
+++ bash/copy_tex.bat
@@ -13,3 +13,19 @@ set mydate=%date:~10,4%_%date:~7,2%_%date:~4,2%_%TIME:~0,2%_%TIME:~3,2%_%TIME:~6
 echo %mydate%
 
 copy manuscript\manuscript.pdf "docs\ms_holeybirds_%mydate%.pdf"
+
+@REM copy the supplementary material to docs
+
+del docs\ms_holeybirds_supplement*.pdf
+
+@REM this directory not included in the repo
+cd supplement-text
+
+pdflatex supplementary-material.tex
+bibtex supplementary-material.aux
+pdflatex supplementary-material.tex
+pdflatex supplementary-material.tex
+
+cd ..
+
+copy supplement-text\supplementary-material.pdf "docs\supplementary_material_holeybirds_%mydate%.pdf"
