5dc9de1be5bd46c91457865fca66c232cfb3861e
Pratik Gupte <pratikgupte16@gmail.com>
2022-12-18 12:36:31 +0000

    Better conda env creation

--- bash/setup_python_env.sh
+++ bash/setup_python_env.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # script to create conda environment
-conda create --name conda_env_holeybirds
+conda create -p ./envs
 
 # command to activate the conda environment
-conda activate conda_env_holeybirds
+conda activate ./envs
 
 # install the geemap package and the earthengine-api package if necessary
 conda install -c conda-forge geemap
 conda install -c conda-forge earthengine-api
 conda install --channel conda-forge geopandas
- conda install -c conda-forge rasterio
+conda install -c conda-forge rasterio
+conda install -c conda-forge rioxarray
 
 # from the conda terminal install the ipython kernel to use with notebooks
 python -m ipykernel install
