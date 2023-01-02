8d7b455edfebbcfe6946021efb5ca276a823b8ca
Pratik Gupte <pratikgupte16@gmail.com>
2023-01-02 19:10:20 +0000

    Update map theme'

--- R/theme_custom_maps.R
+++ R/theme_custom_maps.R
@@ -1,5 +1,7 @@
 
-theme_custom_maps = ggplot2::theme_test() +
+theme_custom_maps = ggplot2::theme_test(
+    base_family = "Arial"
+) +
     ggplot2::theme(
         axis.text = element_blank(),
         axis.title = element_blank()
